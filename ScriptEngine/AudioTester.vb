a79c4d166bf7aca1cb55a5597b4d9c45e9cad4f4
MyRockManuel <michaelMozdzierz@ymail.com>
2020-01-03 20:08:01 -0500

    Added forgotten files

---
+++ ScriptEngine/AudioTester.vb
@@ -0,0 +1,17 @@
+﻿Imports FPX
+Imports Microsoft.Xna.Framework
+
+Public Class AudioTester
+    Inherits Component
+
+    Private keyDown As Boolean
+
+    Public Sub Update(gameTime As GameTime)
+        If (Input.Keyboard.GetState().IsKeyDown(Input.Keys.A) And Not keyDown) Then
+            GetComponent(Of AudioSource).Play()
+        End If
+
+        keyDown = Input.Keyboard.GetState().IsKeyDown(Input.Keys.A)
+    End Sub
+
+End Class

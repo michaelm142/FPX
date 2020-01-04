7124621345b4210e677b5e5ccdcb44b2b1697b6e
MyRockManuel <michaelMozdzierz@ymail.com>
2020-01-03 20:07:01 -0500

    Added forgotten files

---
+++ ComponentModel/Editors/AmbientLightEditor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FPX
+{
+    public partial class AmbientLightEditor : ComponentEditor
+    {
+        public AmbientLightEditor(AmbientLight Target)
+        {
+            InitializeComponent();
+            this.Target = Target;
+            colorPicker1.XnaColor = Target.DiffuseColor;
+            colorPicker1.ColorChanged += ColorPicker1_ColorChanged;
+        }
+
+        private void ColorPicker1_ColorChanged(object sender, EventArgs e)
+        {
+            (Target as AmbientLight).DiffuseColor = colorPicker1.XnaColor;
+        }
+    }
+}
